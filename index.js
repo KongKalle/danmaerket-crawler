17a4e2116e953e578c2b8f166f4977fde4c1ccf5
KongKalle <kaf@wack.dk>
2025-04-23 13:11:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const puppeteer = require('puppeteer');
 async function fetchHtml(url) {
   let browser;
   try {
-    browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
+    browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox'],
+      executablePath: '/usr/bin/google-chrome-stable' // Brug evt. '/usr/bin/chromium-browser' hvis den anden ikke virker
+    });
+
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: 'networkidle2', timeout: 20000 });
 
